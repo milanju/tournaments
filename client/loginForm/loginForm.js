6cf679740861190231a7a37896aa48504dc37ffc
milanju <Milan.Jurhan@live.de>
2015-09-04 03:14:14 +0200

    Add login-form

---
+++ client/loginForm/loginForm.js
@@ -0,0 +1,8 @@
+Template.loginForm.events({
+  'submit #login-form': function(event) {
+    var username = event.target['login-username'].value;
+    var password = event.target['login-password'].value;
+    Meteor.loginWithPassword(username, password);
+    return false;
+  }
+});
