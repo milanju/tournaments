1b280de29db695e4204571b3b784a7e8d24ad8c5
milanju <Milan.Jurhan@live.de>
2015-09-05 08:29:20 +0200

    Add bracket

---
+++ client/tournament/tournamentBracket/tournamentBracket.js
@@ -0,0 +1,16 @@
+Template.tournamentBracket.helpers({
+  bracket: function() {
+    function compare(a, b) {
+      if(a.ro > b.ro)
+        return -1;
+      if(a.ro < b.ro)
+        return 1;
+      return 0;
+    }
+    return this.bracket.sort(compare);
+  },
+  isEven: function() {
+    if(Template.parentData().participants.indexOf(this) % 2 === 0) return true;
+    return false;
+  }
+});
