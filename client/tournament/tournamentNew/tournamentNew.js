13933d45063c2b8d400e555f3d7d3d0a22d8644d
milanju <Milan.Jurhan@live.de>
2015-09-06 06:53:17 +0200

    Add tournament modes

--- client/tournament/tournamentNew/tournamentNew.js
+++ client/tournament/tournamentNew/tournamentNew.js
@@ -1,11 +1,32 @@
+Template.tournamentNew.onCreated(function() {
+  this.modes = new ReactiveVar([]);
+});
+
+Template.tournamentNew.helpers({
+  modes: function() {
+    return Template.instance().modes.get();
+  }
+});
+
 Template.tournamentNew.events({
   'submit #new-tournament-form': function(event) {
+    console.log('submitting');
     var region = event.target['new-tournament-region'].value.toLowerCase();
     var title = event.target['new-tournament-title'].value;
     var category = event.target['new-tournament-category'].value;
     var description = event.target['new-tournament-description'].value;
     var leagues = [event.target['new-tournament-leagues'].value];
     var date = new Date();
+    var modeInputs = $('.new-tournament-mode-custom');
+    var defaultMode = event.target['new-tournament-mode-default'].value;
+    var modes = [];
+    var ro = 2;
+    modes.push({ro: 0, bo: parseInt(defaultMode)});
+    for(var i = 0; i < modeInputs.length; i++) {
+      modes.push({ro: ro, bo: parseInt(modeInputs[i].value)});
+      ro *= 2;
+    }
+    console.log(modes);
 
     Meteor.call('tournamentsCreate', {
       region: region,
@@ -13,7 +34,8 @@ Template.tournamentNew.events({
       category: category,
       description: description,
       leagues: leagues,
-      date: date
+      date: date,
+      modes: modes
     }, function(err, res) {
       if(err) {
         $('.new-tournament-form-error').html(err.reason).show();
@@ -23,5 +45,19 @@ Template.tournamentNew.events({
     });
 
     return false;
+  },
+  'click #new-tournament-mode-add': function(event, template) {
+    var modes = template.modes.get();
+    if(modes.length === 0) {
+      modes.push(2);
+    } else {
+      modes.push(modes[modes.length-1] * 2);
+    }
+    template.modes.set(modes);
+  },
+  'click #new-tournament-mode-remove': function(event, template) {
+    var modes = template.modes.get();
+    modes.pop();
+    template.modes.set(modes);
   }
 });
