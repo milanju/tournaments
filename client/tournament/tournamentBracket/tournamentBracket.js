ad679943564e662eeada39e7fb48e8ecff8a4740
milanju <Milan.Jurhan@live.de>
2015-09-08 07:29:35 +0200

    Finish Bracket Style

--- client/tournament/tournamentBracket/tournamentBracket.js
+++ client/tournament/tournamentBracket/tournamentBracket.js
@@ -27,7 +27,7 @@ Template.tournamentBracket.helpers({
     var margin = 36;
     if(bracketIndex === bracket.length-1) {
       // winner
-      return "0";
+      return '0';
     }
     for(var i = 0; i < bracketIndex; i++) {
       margin *= 2;
@@ -35,12 +35,60 @@ Template.tournamentBracket.helpers({
     margin -= 24;
     if(currentBracket.participants.indexOf(this) === 0
       || currentBracket.participants.indexOf(this) === currentBracket.participants.length-1) {
-      return margin + "px";
+      return margin + 'px';
     } else {
-      return 2*margin + "px";
+      return 2*margin + 'px';
+    }
+  },
+  lineHeight: function() {
+    var bracket = Template.parentData(2).bracket;
+    var currentBracket = Template.parentData();
+    var bracketIndex = bracket.indexOf(currentBracket);
+    var margin = 36;
+
+    if(currentBracket.ro === 2) {
+      return '1px';
+    }
+
+    if(currentBracket.ro === 1) {
+      return '0; border: 0';
+    }
+
+    for(var i = 0; i < bracketIndex; i++) {
+      margin *= 2;
+    }
+
+    margin -= 24;
+    return margin + 'px';
+  },
+  rightTopLineAdjust: function() {
+    var bracket = Template.parentData(2).bracket;
+    var currentBracket = Template.parentData();
+
+    if(bracket.indexOf(currentBracket) === 0) {
+      return 'display: none;';
+    }
+    
+    if(currentBracket.ro === 1) {
+      return 'border-left: 0;';
+    }
+  },
+  rightBottomLineAdjust: function() {
+    var bracket = Template.parentData(2).bracket;
+    var currentBracket = Template.parentData();
+
+    if(bracket.indexOf(currentBracket) === 0) {
+      return 'display: none;';
+    }
+
+    if(currentBracket.ro === 1) {
+      return 'display: none;';
     }
   },
   marginBottom: function() {
     return "";
+  },
+  isFourth: function() {
+    return Template.parentData().participants.indexOf(this) % 4 === 0
   }
 });
